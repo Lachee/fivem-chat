45a2cad7bf24a0a021fda5554768eec4ffdf5ef9
Lachee <lachlan.h@hotmail.com>
2021-04-22 16:07:00 +1000

    Updated deets

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# This entire file is mostly pointless because FiveM rebuilds it every start anyways
+
 echo "Deploying Lachee's Chat...";
 echo " - Updating Files";
 git pull
 
 echo " - Insuring Deps";
-npm i
+rm yarn.lock            # Yarn is annoying in this case. If we update our deps then this will fail.
+rm .yarn.installed      
+npm i                   # Update our deps.
 
 echo " - Building...";
 rm -fR ./dist/
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,10 @@
 -- This resource is part of the default Cfx.re asset pack (cfx-server-data)
 -- Altering or recreating for local use only is strongly discouraged.
 
-version '1.0.0'
-author 'Cfx.re <root@cfx.re>'
-description 'Provides baseline chat functionality using a NUI-based interface.'
-repository 'https://github.com/citizenfx/cfx-server-data'
+version '1.0.2'
+author 'Lachee, Cfx.re <root@cfx.re>'
+description 'Modified version of the base chat.'
+repository 'https://github.com/lachee/fivem-chat'
 
 ui_page 'dist/ui.html'
 
@@ -14,8 +14,7 @@ server_script 'sv_chat.lua'
 files {
   'dist/ui.html',
   'dist/index.css',
-  'html/vendor/*.css',
-  'html/vendor/fonts/*.woff2',
+  'dist/fonts/*.woff2',
 }
 
 fx_version 'adamant'
@@ -23,7 +22,6 @@ games { 'rdr3', 'gta5' }
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 dependencies {
-  'yarn',
   'webpack'
 }
 
